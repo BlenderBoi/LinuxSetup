589eabe528fa18566bf3d6781e2c59b6a26362a1
blenderboi <dummy@dummy.com>
2022-03-06 18:58:29 +0800

    Update

--- installs/install_neovim.sh
+++ installs/install_neovim.sh
@@ -4,6 +4,7 @@ echo "Install Neovim"
 sudo pacman -S neovim
 echo "copy ~/.config/nvim"
 cp -f -r .config/nvim ~/.config/
+cp -f -r Desktop-Files/nvim.desktop ~/.local/share/applications/
 
 echo "Install XClip"
 sudo pacman -S xclip
